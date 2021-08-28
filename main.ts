22fded8e4f18cc4709638da7be2737b835cbf7c8
Chetachi Ezikeuzor <79069364+chetachiezikeuzor@users.noreply.github.com>
2021-08-27 21:55:30 -0500

    👩🏽‍💻 Configured functions

--- main.ts
+++ main.ts
@@ -1,112 +1,64 @@
-import { App, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Notice, addIcon, setIcon, Plugin } from "obsidian";
 
 interface MyPluginSettings {
-	mySetting: string;
+  mySetting: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+  mySetting: "default",
+};
+
+const icons: Record<string, string> = {
+  yinyang: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" stroke-width="0" stroke-linecap="round" stroke-linejoin="round"><path d="M19.062 4.938A9.942 9.942 0 0 0 12.016 2h-.026a9.94 9.94 0 0 0-7.071 2.938c-3.898 3.898-3.898 10.243 0 14.143c1.895 1.895 4.405 2.938 7.071 2.938s5.177-1.043 7.071-2.938c3.9-3.899 3.9-10.243.001-14.143zM13.5 15a1.5 1.5 0 1 1-.001 3.001A1.5 1.5 0 0 1 13.5 15zM6.333 6.353A7.953 7.953 0 0 1 11.99 4l.026.001c1.652.008 3.242 1.066 3.55 2.371c.366 1.552-1.098 3.278-4.018 4.737c-5.113 2.555-5.312 5.333-4.975 6.762l.008.021c-.082-.075-.169-.146-.249-.226c-3.118-3.119-3.118-8.194.001-11.313z" fill="currentColor"/><circle cx="10.5" cy="7.5" r="1.5" fill="currentColor"/></svg>`,
+};
+
+function addIcons() {
+  Object.keys(icons).forEach((key) => {
+    addIcon(key, icons[key]);
+  });
 }
 
 export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  settings: MyPluginSettings;
+  statusBarIcon: HTMLElement;
 
-	async onload() {
-		console.log('loading plugin');
+  async onload() {
+    console.log("LightMode DarkMode v" + this.manifest.version + " loaded");
+    addIcons();
+    await this.loadSettings();
+    this.app.workspace.onLayoutReady(() => {
+      setTimeout(() => {
+        this.setupStatusBar();
+      });
+    });
+  }
 
-		await this.loadSettings();
+  setupStatusBar() {
+    this.statusBarIcon = this.addStatusBarItem();
+    this.statusBarIcon.addClass("LD-statusbar-button");
+    setIcon(this.statusBarIcon, "yinyang");
 
-		this.addRibbonIcon('dice', 'Sample Plugin', () => {
-			new Notice('This is a notice!');
-		});
+    this.registerDomEvent(this.statusBarIcon, "click", () => {
+      //@ts-ignore
+      this.app.getTheme() === "obsidian"
+        ? //@ts-ignore
+          this.app.commands.executeCommandById("theme:use-light") +
+          new Notice("☀️")
+        : //@ts-ignore
+          this.app.commands.executeCommandById("theme:use-dark") +
+          new Notice("🌑");
+    });
+  }
 
-		this.addStatusBarItem().setText('Status Bar Text');
+  onunload() {
+    console.log("LightMode DarkMode unloaded");
+  }
 
-		this.addCommand({
-			id: 'open-sample-modal',
-			name: 'Open Sample Modal',
-			// callback: () => {
-			// 	console.log('Simple Callback');
-			// },
-			checkCallback: (checking: boolean) => {
-				let leaf = this.app.workspace.activeLeaf;
-				if (leaf) {
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-					return true;
-				}
-				return false;
-			}
-		});
+  async loadSettings() {
+    this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+  }
 
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		this.registerCodeMirror((cm: CodeMirror.Editor) => {
-			console.log('codemirror', cm);
-		});
-
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-		console.log('unloading plugin');
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		let {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		let {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		let {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue('')
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  async saveSettings() {
+    await this.saveData(this.settings);
+  }
 }
